158fa2576cfb808ffb07c99645e5e7f5a17b7017
Michał Jasiński <jasinskimichal.1996@gmail.com>
2023-01-11 16:23:36 +0100

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const generateGallery = (galleryItem) => {
+  const galleryImage = document.createElement("img");
+
+  galleryImage.classList.add("gallery__image");
+  galleryImage.setAttribute("src", galleryItem.preview);
+  galleryImage.setAttribute("alt", galleryItem.description);
+  galleryImage.dataset.source = galleryItem.original;
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.setAttribute("href", galleryItem.original);
+  galleryLink.append(galleryImage);
+
+  const galleryContainter = document.createElement("div");
+  galleryContainter.classList.add("gallery__item");
+  galleryContainter.append(galleryLink);
+
+  return galleryContainter;
+};
+
+const generateImages = (galleryItems) => {
+  galleryItems.forEach((item) => {
+    gallery.append(generateGallery(item));
+  });
+};
+generateImages(galleryItems);
+
+let lightbox;
+
+const linkClick = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("gallery__image")) {
+    lightbox = basicLightbox.create(
+      `<img width="1400" height="900" src=${e.target.dataset.source}>`
+    );
+    lightbox.show();
+  }
+};
+const closeLightBoxWithEsc = (e) => {
+  if (e.key === "Escape" && basicLightbox.visible()) {
+    lightbox.close();
+  }
+};
+gallery.addEventListener("click", linkClick);
+gallery.addEventListener("keydown", closeLightBoxWithEsc);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const generateGallery = (galleryItem) => {
+  const galleryImage = document.createElement("img");
+
+  galleryImage.classList.add("gallery__image");
+  galleryImage.setAttribute("src", galleryItem.preview);
+  galleryImage.setAttribute("alt", galleryItem.description);
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.setAttribute("href", galleryItem.original);
+  galleryLink.append(galleryImage);
+
+
+
+  return galleryLink;
+};
+
+const generateImages = (galleryItems) => {
+  galleryItems.forEach((item) => {
+    gallery.append(generateGallery(item));
+  });
+};
+generateImages(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
